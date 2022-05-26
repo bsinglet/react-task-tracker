60223960ce78e7713690d49c7eb6ef75221e856e
Benjamin M. Singleton <bsinglet@vt.edu>
2022-05-25 21:46:13 -0400

    Added the first component!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-function App() {
-  const name = 'Brad'
-  const x = true
+import Header from './components/Header'
 
+function App() {
   return (
     <div className="container">
-      <h1>Hello From React</h1>
-      <h2>Hello {name}</h2>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+      <Header />
     </div>
   );
 }
